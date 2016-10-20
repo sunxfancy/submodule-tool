6a2fa7c0aca20004628deaf958d9bdede4173e30
rickie120243 <rickie120243@gmail.com>
2016-10-20 15:56:08 +0800

    add if condition

--- src/submodule.sh
+++ src/submodule.sh
@@ -2,11 +2,18 @@
 BASE_PATH=$(pwd)
 SUB_PATHS=$(cat ./.gitmodules | grep path | awk '{print $3}')
 
-for path in $SUB_PATHS
-do
+for path in $SUB_PATHS; do
   test -d "$path" || continue
   cd $path
-  echo "npm i in "$path
-  npm i
-  cd "${BASE_PATH}"
+  if test -e "package.json"; then
+    if cat package.json | grep -iq dependencies; then
+      echo "npm i in "$path
+      npm i
+      cd "${BASE_PATH}"
+    else
+      echo "no dependencies in "$path
+    fi
+  else
+    echo "no package.json in "$path
+  fi
 done
