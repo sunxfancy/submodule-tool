6c2a439b51225197c6372d12e107e50a774e1b5e
rickie120243 <rickie120243@gmail.com>
2016-10-20 16:24:09 +0800

    add return to base path

--- src/submodule.sh
+++ src/submodule.sh
@@ -4,6 +4,7 @@ SUB_PATHS=$(cat ./.gitmodules | grep path | awk '{print $3}')
 
 for path in $SUB_PATHS; do
   test -d "$path" || continue
+  echo $path
   cd $path
   if test -e "package.json"; then
     if cat package.json | grep -iq dependencies; then
@@ -12,8 +13,10 @@ for path in $SUB_PATHS; do
       cd "${BASE_PATH}"
     else
       echo "no dependencies in "$path
+      cd "${BASE_PATH}"
     fi
   else
     echo "no package.json in "$path
+    cd "${BASE_PATH}"
   fi
 done
