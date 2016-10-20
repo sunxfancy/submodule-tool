39a1da136498034dfc76be8939eb06af7b490e32
rickie120243 <rickie120243@gmail.com>
2016-10-20 14:20:23 +0800

    add loop to do npm i in every directory

--- src/submodule.sh
+++ src/submodule.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
 BASE_PATH=$(pwd)
 SUB_PATH=$(cat ./.gitmodules | grep path | awk '{print $3}')
 echo "${SUB_PATH}"
 echo "${BASE_PATH}"
+
+for path in $SUB_PATH
+do
+  test -d "$path" || continue
+  cd $path
+  echo "npm i in "$path
+  npm i
+  cd "${BASE_PATH}"
+done
