174f3d40997045ab17bff086f3496614bb78e810
rickie120243 <rickie120243@gmail.com>
2016-10-20 16:57:46 +0800

    add if .gitmodule exist

--- src/submodule.sh
+++ src/submodule.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-BASE_PATH=$(pwd)
-SUB_PATHS=$(cat ./.gitmodules | grep path | awk '{print $3}')
-
-for path in $SUB_PATHS; do
-  test -d "$path" || continue
-  echo $path
-  cd $path
-  if test -e "package.json"; then
-    if cat package.json | grep -iq dependencies; then
-      echo "npm i in "$path
-      npm i
-      cd "${BASE_PATH}"
+function installSub(){
+  BASE_PATH=$(pwd)
+  SUB_PATHS=$(cat ./.gitmodules | grep path | awk '{print $3}')
+  for path in $SUB_PATHS; do
+    test -d "$path" || continue
+    cd $path
+    if test -e "package.json"; then
+      if cat package.json | grep -iq dependencies; then
+        echo "npm installing "$path
+        npm i
+        cd "$BASE_PATH"
+      else
+        echo "no dependencies in package.json" $path
+        cd "$BASE_PATH"
+      fi
     else
-      echo "no dependencies in "$path
-      cd "${BASE_PATH}"
+      echo "no package.json "$path
+      cd "$BASE_PATH"
     fi
-  else
-    echo "no package.json in "$path
-    cd "${BASE_PATH}"
-  fi
-done
+  done
+}
+
+if test -e ".gitmodules"; then
+  installSub
+else
+  echo "no .gitmodules in "$(pwd)
+fi
+
+
+
